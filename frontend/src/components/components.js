add6dd9de4021cbbea560cc6405d88a0941e4255
Wanomir <ioan.romadin@gmail.com>
2024-06-27 10:17:46 +0300

    feat: add components and set up routes

---
+++ frontend/src/components/components.js
@@ -0,0 +1,12 @@
+import ErrorPage from "./ErrorPage.jsx";
+import Home from "./Home.jsx";
+import Movies from "./Movies.jsx";
+import Genres from "./Genres.jsx";
+import EditMovie from "./EditMovie.jsx";
+import ManageCatalogue from "./ManageCatalogue.jsx";
+import GraphQL from "./GraphQL.jsx";
+import Login from "./Login.jsx";
+
+export {
+	ErrorPage, Home, Movies, Genres, EditMovie, ManageCatalogue, GraphQL, Login
+}
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,17 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import HelloWorld from "./HelloWorld.jsx"
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import App from './App'
 import './index.css'
+import * as cmp from './components/components.js'
 
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <App/>,
+		errorElement: <cmp.ErrorPage/>,
+		children: [
+			{index: true, element: <cmp.Home/>},
+			{path: "/movies", element: <cmp.Movies/>,},
+			{path: "/genres", element: <cmp.Genres/>,},
+			{path: "/admin/movie/0", element: <cmp.EditMovie/>,},
+			{path: "/manage-catalogue", element: <cmp.ManageCatalogue/>,},
+			{path: "/graphql", element: <cmp.GraphQL/>,},
+			{path: "/login", element: <cmp.Login/>,},
+		]
+	},
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <div className={"container"}>
-            <div className={"row"}>
-                <div className={"col"}>
-                    <HelloWorld msg={"Hello, World!"}/>
-                </div>
-            </div>
-        </div>
-    </React.StrictMode>,
+	<React.StrictMode>
+		<RouterProvider router={router}/>
+	</React.StrictMode>,
 )
